98dbddd4d7c7afa31f3c04c05231fecada223f74
epswartz <eswartzen@gmail.com>
2021-06-24 21:39:42 -0400

    all tests in one dir, test multi return

--- integration_test.py
+++ integration_test.py
@@ -26,32 +26,38 @@ class TestObject:
         return hash(repr(self))
 
 
-@file_cache("./test_int_noarg")
+@file_cache("./cache_dir/test_int_noarg")
 def test_int_noarg():
     global exec_count
     exec_count += 1
     return 1
 
 
-@file_cache("./test_object_noarg")
+@file_cache("./cache_dir/test_object_noarg")
 def test_object_noarg():
     global exec_count
     exec_count += 1
     return 1
 
 
-@file_cache("./test_int_arg")
+@file_cache("./cache_dir/test_int_arg")
 def test_int_arg(x):
     global exec_count
     exec_count += 1
     return x
 
 
-@file_cache("./test_object_arg")
+@file_cache("./cache_dir/test_object_arg")
 def test_object_arg(o):
     global exec_count
     exec_count += 1
     return o
+
+@file_cache("./cache_dir/test_multi_return")
+def test_multi_return():
+    global exec_count
+    exec_count += 1
+    return 1,2
 
 
 run_number = int(sys.argv[1])
@@ -61,10 +67,14 @@ if run_number == 1:  # First run, execute functions and populate file cache.
     test_object_noarg()
     test_int_arg(1)
     test_object_arg(TestObject(2))
-    assert exec_count == 4
+    test_multi_return()
+    assert exec_count == 5
 elif run_number == 2:  # Second run, execute functions, check return vals and that there were no executions.
     assert test_int_noarg() == 1
     assert test_object_noarg() == 1
     assert test_int_arg(1) == 1
     assert test_object_arg(TestObject(2)) == TestObject(2)
+    a,b = test_multi_return()
+    assert a == 1
+    assert b == 2
     assert exec_count == 0
